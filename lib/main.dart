012d6f19eb4d123fce87fa9e4ac6e99fddcc6023
Dwiki Nandhika <dpasanandhika@gmail.com>
2024-09-30 12:42:09 +0700

    Update1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Kuis PPAM",
+      title: "Kuis",
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.purpleAccent,
